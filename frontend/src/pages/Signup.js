3185ea28fe97327ce97b4eab545c529f9f71b36b
Nehabhavsar17 <143259680+Nehabhavsar17@users.noreply.github.com>
2025-01-19 14:45:21 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -33,7 +33,7 @@ function Signup() {
         }
 
         try {
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://deploy-mern-app-2-ui1.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
