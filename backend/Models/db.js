1e2985c3e1dd94bbbc63d602be15aa989fd562f9
Nehabhavsar17 <143259680+Nehabhavsar17@users.noreply.github.com>
2025-01-19 23:06:05 +0530

    Update db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,7 +1,7 @@
 const  mongoose=require('mongoose');
 const mongo_url =process.env.MONGO_CONN;
 mongoose.connect(mongo_url)
-    .then(() => {
+    .then(() => { 
         console.log('MongoDB Connected...');
     }).catch((err) => {
         console.log('MongoDB Connection Error: ', err);
