babb1c1d2c907432b50625cf086d73f1ab06202e
Nehabhavsar17 <143259680+Nehabhavsar17@users.noreply.github.com>
2025-01-20 09:17:18 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -32,10 +32,7 @@ function Login() {
         }
 
         try {
-            
-           // const url = `https://deploy-mern-app-2-api2.vercel.app/login`; // Change to HTTPS in production
-            const url = `https://deploy-mern-app-2-api1.vercel.app/auth/login`;
-            //const url = `https://deploy-mern-app-1-api.vercel.app/auth/login`;
+            const url = "http://localhost:8080/auth/login"; 
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
