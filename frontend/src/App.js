d62740ec38726cc555eba7eb94f5511a82dd02f6
Nehabhavsar17 <143259680+Nehabhavsar17@users.noreply.github.com>
2025-01-20 21:34:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
@@ -5,6 +6,7 @@ import Signup from './pages/Signup';
 import Home from './pages/Home';
 import { useState } from 'react';
 import RefrshHandler from './RefrshHandler';
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
