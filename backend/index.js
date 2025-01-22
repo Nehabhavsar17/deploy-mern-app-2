69c10b16e5a6ac30dc5a2840e2ab96a84e3f09f5
Nehabhavsar17 <143259680+Nehabhavsar17@users.noreply.github.com>
2025-01-22 08:43:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,37 @@
-const express=require('express')
-const app=express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
-const ProductRouter = require('./Routes/ProductRouter');
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const ProductRouter = require("./Routes/ProductRouter");
 
-require('./Models/User');
-require('dotenv').config();
-require('./Models/db')
+require("./Models/User");
+require("dotenv").config();
+require("./Models/db");
 
-const PORT=process.env.PORT||8080;
-app.get('/ping',(req,res)=>{
-    res.send('PONG');
-})
+app.use(express.json());
+const PORT = process.env.PORT || 8080;
+
+app.get("/ping", (req, res) => {
+  res.send("PONG");
+});
+
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/auth', AuthRouter);
-app.use('/products', ProductRouter);
+app.use("/auth", AuthRouter);
+app.use("/products", ProductRouter);
+app.get("/image/:filename", (req, res) => {
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, "uploads", filename);
 
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).json({ message: "Image not found", success: false });
+    }
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
