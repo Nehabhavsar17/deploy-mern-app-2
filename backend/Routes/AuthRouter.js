13f6df233c534b9003272241f2d96a853fc94ba9
Nehabhavsar17 <143259680+Nehabhavsar17@users.noreply.github.com>
2025-01-22 09:10:30 +0530

    Update AuthRouter.js

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -2,8 +2,6 @@ const { signup, login } = require('../Controllers/AuthController');
 const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
 const upload = require('../utils/multer'); 
 const router = require('express').Router();
- 
 router.post('/login', loginValidation, login);
 router.post('/signup', upload.single('profileImage'), signupValidation, signup);
-
 module.exports = router;
